09d07b9cbf73d3904bd6145370f6605dd8ffeb8d
Alexey Staburov <gilbertfrost@yandex.ru>
2026-07-31 16:18:25 +0300

    Fix texture paths for GitHub Pages

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import App from './App.vue'
 
 import { textureLoader } from './game/three/loaders/TextureLoader.ts'
 await textureLoader.loadAll({
-  ice: '/texture/ice128px.png',
-  fire: '/texture/fire128px.png',
-  earth: '/texture/earth128px.png',
-  dark: '/texture/dark128px.png',
-  light: '/texture/light128px.png',
+  ice: `${import.meta.env.BASE_URL}texture/ice128px.png`,
+  fire: `${import.meta.env.BASE_URL}texture/fire128px.png`,
+  earth: `${import.meta.env.BASE_URL}texture/earth128px.png`,
+  dark: `${import.meta.env.BASE_URL}texture/dark128px.png`,
+  light: `${import.meta.env.BASE_URL}texture/light128px.png`,
 })
 
 
